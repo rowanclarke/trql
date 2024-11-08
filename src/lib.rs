0fecf8eefed428d1decf72b9f29b957d9f0cd376
Rowan Clarke <r.n.clarke@lancaster.ac.uk>
2024-11-08 13:28:06 +0000

    Added Condition Implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use parser::{Operation, Select};
-use tree::{Children, Condition, Descendants, Tree};
+use parser::{Operation, Select, Series};
+use tree::{Chain, Children, Condition, Descendants, DynNodes, Nodes, Tree};
 
 #[macro_use]
 extern crate pest_derive;
@@ -11,20 +11,37 @@ mod tree;
 mod tests;
 
 pub trait Command {
-    fn execute<T: Tree + 'static, I: Iterator<Item = T::Node> + 'static>(
-        self,
-        iter: I,
-    ) -> Box<dyn Iterator<Item = T::Node>>;
+    fn execute<T: Tree + 'static, I: Nodes<T> + 'static>(self, iter: I) -> Box<dyn DynNodes<T>>;
+}
+
+impl Command for Select {
+    fn execute<T: Tree + 'static, I: Nodes<T> + 'static>(self, iter: I) -> Box<dyn DynNodes<T>> {
+        Box::new(Chain::<T>::new(
+            self.into_iter()
+                .map(|x| x.execute::<T, I>(iter.clone()))
+                .collect(),
+        ))
+    }
+}
+
+impl Command for Series {
+    fn execute<T: Tree + 'static, I: Nodes<T> + 'static>(self, iter: I) -> Box<dyn DynNodes<T>> {
+        let mut iter = Box::new(iter) as Box<dyn DynNodes<T>>;
+        for command in self {
+            iter = command.execute::<T, Box<dyn DynNodes<T>>>(iter);
+        }
+        iter
+    }
 }
 
 impl Command for Operation {
-    fn execute<T: Tree + 'static, I: Iterator<Item = T::Node> + 'static>(
-        self,
-        iter: I,
-    ) -> Box<dyn Iterator<Item = T::Node>> {
+    fn execute<T: Tree + 'static, I: Nodes<T> + 'static>(self, iter: I) -> Box<dyn DynNodes<T>> {
         match self {
             Self::Children => Box::new(Children::<T, I>::new(iter)),
             Self::Descendants => Box::new(Descendants::<T, I>::new(iter)),
+            Self::Condition(select) => Box::new(Condition::new(iter, move |tree: T| {
+                select.clone().execute::<T, T>(tree)
+            })),
             _ => todo!(),
         }
     }
